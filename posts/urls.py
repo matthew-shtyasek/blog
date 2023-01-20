f05c55683aa92093ea23a3a1b67a561b2c1d56d4
33group <paulprozxc@mail.ru>
2023-01-20 13:03:58 +0500

    Вывод поста и дизайн вверхнего меню

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from posts.views import post_list_view
+from posts.views import post_list_view, post_id_view
 
 app_name="posts"
-urlpatterns=[path("",post_list_view)]
+urlpatterns=[
+    path("",post_list_view),
+    path("post/<int:pk>", post_id_view, name="post"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from posts.models import Post
 
 
 def post_list_view (request):
     posts=Post.objects.filter(published=True)
-    context={"post_list": posts}
+    context={"post_list": posts,'page':'posts'}
     return render(request, "posts/post_list.html",context)
-# Create your views here.
+
+def post_id_view(request, pk):
+    pk = get_object_or_404(Post, pk=pk)
+    context = {"pk": pk,'page':'posts'}
+    return render(request, "posts/post_id.html", context)
