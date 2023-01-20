1fe06f0e76a6fc305ee4a54beec57c9a735f29a4
33group <paulprozxc@mail.ru>
2023-01-20 10:37:46 +0500

    Создание оброботчика

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
+
+def post_list_view (request):
+    posts=Post.objects.filter(published=True)
+    context={"post_list": posts}
+    return render(request, "posts/post_list.html",context)
 # Create your views here.
