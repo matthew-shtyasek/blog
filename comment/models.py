44d32cb06db0ac6c3206be021a573101a2d95b29
33group <paulprozxc@mail.ru>
2023-01-28 09:35:36 +0500

    создали модель комментария

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from posts.models import Post
 
+
+class Comment(models.Model):
+    text = models.TextField(blank=False) #нельзя отправлять пустой комментарий
+    date_of_create = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=32)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-date_of_create', )
