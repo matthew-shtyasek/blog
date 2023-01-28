cb187a19210f8d4473af027433b8ae2483987a35
33group <paulprozxc@mail.ru>
2023-01-28 11:04:44 +0500

    форма отправляется методом post

--- posts/views.py
+++ posts/views.py
@@ -13,13 +13,14 @@ def post_list_view (request):
 def post_id_view(request, pk):
     pk = get_object_or_404(Post, pk=pk)
     comments = Comment.objects.filter(post = pk, published=True)
-    if request.method=="GET": #если метод запроса GET
-        form = CommentForm(request.GET) #то мы передаём запрос в форму
+    if request.method=="POST": #если метод запроса POST
+        form = CommentForm(request.POST) #то мы передаём запрос в форму
         if form.is_valid(): #если форма заполнена корректно
             comment =  form.save(commit=False) #получаем объект комментария без сохранения
             comment.post = pk #записываем к какому посту относится данный комментарий
             comment.save() #комментарий сохраняется
             form = CommentForm() #создаём новую форму
-
+    else:
+        form = CommentForm()
     context = {"pk": pk,'page':'posts', 'comments_list':comments, 'form':form}
     return render(request, "posts/post_id.html", context)
