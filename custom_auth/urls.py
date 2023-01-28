873a3309ef1b23df11b09fd0ea51f388d8787504
33group <paulprozxc@mail.ru>
2023-01-28 13:59:34 +0500

    сделали авторизацию и регистр. и смену пароля

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,8 +1,16 @@
+import django.views.generic.edit
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import path, reverse_lazy
 
 app_name = 'custom_auth'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view()),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('change_password/', auth_views.PasswordChangeView.as_view() ),
+    #path('change_password/', django.core ),
+    path('gigi/', django.views.generic.edit.CreateView.as_view(form_class=UserCreationForm, template_name='registration/login.html', success_url=reverse_lazy("custom_auth:login"))),
+
+
+
 ]
